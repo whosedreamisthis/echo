2c72fc5fdaaa7199e186b631c4d6272230c18ca0
Dana Sharon <whosedreamisthis@gmail.com>
2026-05-28 10:04:51 -0700

    added sign out button to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import NewThread from "@/components/new-thread";
 import AuthProvider from "@/components/auth-provider";
 import { getSessionUser } from "@/lib/auth-user";
 import { SignOutButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 export default async function Home() {
   const { userId, mongoUserId, mongoProfileImage } = await getSessionUser();
 
@@ -14,21 +15,29 @@ export default async function Home() {
   const { posts, nextCursor } = await getPosts(userId);
 
   return (
-    <div className="w-full flex flex-col justify-center items-center">
-      {!userId && <DemoButton />}
-      <SignOutButton />
-      <div className="w-full max-w-xl sm:rounded-2xl mb-5 overflow-hidden border border-gray-200 bg-white">
-        <div className="border-b border-gray-200">
-          <NewThread profileImage={mongoProfileImage} />
+    <div>
+      <div className="flex items-center justify-end mb-5">
+        {!userId && <DemoButton />}
+        {userId && (
+          <SignOutButton>
+            <Button>Sign Out</Button>
+          </SignOutButton>
+        )}
+      </div>
+      <div className="w-full flex flex-col justify-center items-center">
+        <div className="w-full max-w-xl sm:rounded-2xl mb-5 overflow-hidden border border-gray-200 bg-white">
+          <div className="border-b border-gray-200">
+            <NewThread profileImage={mongoProfileImage} />
+          </div>
+          <AuthProvider userId={mongoUserId}>
+            {/* Pass data properties down to initiate streaming state machine loop */}
+            <Feed
+              initialPosts={posts}
+              initialCursor={nextCursor}
+              currentClerkUserId={userId}
+            />
+          </AuthProvider>
         </div>
-        <AuthProvider userId={mongoUserId}>
-          {/* Pass data properties down to initiate streaming state machine loop */}
-          <Feed
-            initialPosts={posts}
-            initialCursor={nextCursor}
-            currentClerkUserId={userId}
-          />
-        </AuthProvider>
       </div>
     </div>
   );
